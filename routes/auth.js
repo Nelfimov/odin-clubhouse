7b7fb49f5f688e0265671f0ef8afffbf3b45483a
Nelfimov <nelfimov@mail.ru>
2022-12-09 12:41:39 +0300

    added encryption and sign up

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,31 @@
 import { Router } from 'express';
+import bcrypt from 'bcryptjs';
+import User from '../models/user.js';
 
 const router = Router();
 
-router.get('/sign-up', (req, res, next) => {
-  res.render('pages/signup-form', {title: 'Sign Up'});
-}).post('/sign-up', () => {
+router
+  .get('/sign-up', (req, res, next) => {
+    res.render('pages/signup-form', {title: 'Sign Up'});
+})
+  .post('/sign-up', (req, res, next) => {
+    const {password} = req.body;
+    bcrypt.hash(password, 10, (err, hashedPassword) => {
+      if (err) return next(err);
 
+      const {firstName, lastName, email} = req.body;
+
+      const newUser = new User({
+        firstName,
+        lastName,
+        email,
+        hashedPassword,
+      }).save((err) => {
+        if (err) return next(err);
+
+        res.redirect('/');
+      });
+  })
 });
 
 export default router;
