c546a0ac62144f8a04f6cb31517a3c824c61b042
Nelfimov <nelfimov@mail.ru>
2022-12-12 17:51:03 +0300

    added rendering messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 import {Router as router} from 'express';
 
+import Message from '../models/message.js';
+
 const customRouter = router();
 
 customRouter.get('/', function(req, res, next) {
-  res.render('pages/index', {title: 'Main'});
+  Message.find().populate('author').lean().then((messages) => {
+    res.render('pages/index', {title: 'Main', allMessages: messages});
+  }).catch((err) => next(err));
 });
 
 export default customRouter;
